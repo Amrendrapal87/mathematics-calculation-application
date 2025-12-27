f7105da28954f72b42fe8477dbbc25553ad0895e
Amrendra pal <amrendrapal7777@gmail.com>
2025-12-27 19:17:18 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,77 @@
+// Area of circle ...//
+
+// Type 1.. 
+
+var area =function(r){
+  return  Math.PI*r*r;
+
+}
+var r=5;
+console.log(area(r));
+
+// Type 2..
+
+// function area(r){
+//     return Math.PI*r*r;
+// }
+
+// console.log(area(2));
+
+// Area of square ...//
+
+// type1.. 
+
+// var area = function(a){
+//     return a*a;
+// }
+
+// var a=2;
+// console.log(area(a));
+
+// type2.. 
+
+// function square(a){
+//     return a*a;
+// }
+
+// console.log(square(2));
+
+
+// Area of rectangle.....//
+
+// TYPE 1..
+
+// var area= function(l,b){
+//     return l*b;
+// }
+//  var l=2;
+//  var b=3;
+//  console.log(area(l,b));
+
+// TYPE 2..
+
+// function rectangle(l,b){
+//     return l*b;
+// }
+
+// console.log(rectangle(2,3));
+
+
+// Area of Triangle ....//
+
+// TYPE 1..
+
+// let area=function(b,h){
+//     return 1/2*b*h;
+// }
+// let b=3;
+// let h=5;
+// console.log(area(b,h));
+
+// TYPE 2..
+
+// function triangle(b,h){
+//     return 1/2*b*h;
+// }
+
+// console.log(triangle(3,5));
